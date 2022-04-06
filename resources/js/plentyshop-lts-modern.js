68241cce4eb47bdc8978b2bfbe28f7551542f14f
Christoph Johannsen <christoph.johannsen@plentymarkets.com>
2022-04-06 15:52:46 +0200

    Merge branch 'stable' into refactor/javascript
    # Conflicts:
    #	resources/js/plenty-shop-light.js

--- resources/js/plentyshop-lts-modern.js
+++ resources/js/plentyshop-lts-modern.js
@@ -1,9 +1,8 @@
 const BG_TRANSPARENT_CLASS = "bg-transparent";
 
 class PlentyShopLTSModern {
-    headerElements = [];
-    negativeMarginElements = [];
     topBarOffsetHeight = 0;
+    bgTransparentElements = [];
 
     constructor() {
         document.addEventListener("DOMContentLoaded", () => this.init());
@@ -28,39 +27,38 @@ class PlentyShopLTSModern {
     getHeaderElementsAndHeights() {
         // TODO: document .negative-margin-top .bg-transparent
         // TODO: alle Widgets berücksichtigen
-        // TODO: this.headerElements = document.querySelectorAll("#page-header-parent > .bg-transparent");
-        this.headerElements = document.querySelectorAll(".top-bar, .navbar, .breadcrumbs");
-        this.negativeMarginElements = document.querySelectorAll(".negative-margin-top");
-        this.topBarOffsetHeight = 0;
+
+        this.bgTransparentElements = document.querySelectorAll("#page-header-parent > .bg-transparent:not(.unfixed)");
+
+        const allheaderElements = document.querySelectorAll("#page-header-parent > *");
+        const fixedElements = allheaderElements.querySelectorAll(":not(.unfixed)");
+        const unfixedElements = allheaderElements.querySelectorAll(".unfixed");
+        
+        const negativeMarginElements = document.querySelectorAll(".negative-margin-top");
         const topBarElement = document.querySelector(".top-bar.unfixed");
-        let headerElementOffsetHeight = 0;
+        let allHeaderElementsHeight = 0;
 
         // set offset based on active header elements
-        this.headerElements.forEach(element => headerElementOffsetHeight += element.offsetHeight);
-        headerElementOffsetHeight = Math.ceil(headerElementOffsetHeight);
+        allheaderElements.forEach(element => allHeaderElementsHeight += element.offsetHeight);
+
+        // add negative margin to specified element
+        negativeMarginElements.forEach(element =>
+            element.style.setProperty("margin-top", -Math.ceil(allHeaderElementsHeight) + "px", "important")
+        );
 
         if (topBarElement) {
             this.topBarOffsetHeight = topBarElement.offsetHeight;
         }
-
-        // add negative margin to specified element
-        this.negativeMarginElements.forEach(element =>
-            element.style.setProperty("margin-top", -headerElementOffsetHeight + "px", "important")
-        );
     }
 
     /**
      * toggle the transparent class on the header elements
      */
     updateHeaderBackgrounds() {
-        // TODO: statt topBarOffsetHeight die höhe der Elemente, die wegscrollen
-        // TODO: nur auf Elementen ausführen, die schon die Klasse haben
-        if (window.pageYOffset > this.topBarOffsetHeight) {
-            this.headerElements.forEach(element => element.classList.remove(BG_TRANSPARENT_CLASS));
-        }
-        else {
-            this.headerElements.forEach(element => element.classList.add(BG_TRANSPARENT_CLASS));
-        }
+        // TODO: statt topBaxrOffsetHeight die höhe der Elemente, die wegscrollen
+        // TODO: wert renamen
+        const wert = window.pageYOffset > this.topBarOffsetHeight;
+        this.bgTransparentElements.forEach(element => element.classList.toggle(BG_TRANSPARENT_CLASS, wert));
     }
 }
 
