15ed425fa13f7e8dea9509fefb52500402697969
Daniel Gaiduk <daniel.gaiduk@plentymarkets.com>
2022-04-06 17:58:01 +0200

    *

--- resources/js/src/plentyShopLTSModern.js
+++ resources/js/src/plentyShopLTSModern.js
@@ -14,21 +14,29 @@ export default class PlentyShopLTSModern {
 
         this.updateHeaderBackgrounds();
 
+        // add scroll listener for dynamic menu state
         document.addEventListener("scroll", this.updateHeaderBackgrounds.bind(this));
     }
 
+    /**
+     * collect all elements in header
+     */
     getElements() {
-        this.headerElements = [...document.querySelectorAll("#page-header-parent > *")];
-        this.unfixedHeaderElements = [...document.querySelectorAll("#page-header-parent > .bg-transparent:not(.unfixed)")];
-        this.negativeMarginElements = [...document.querySelectorAll(".negative-margin-top")];
+        this.headerElements = document.querySelectorAll("#page-header-parent > *");
+        this.unfixedHeaderElements = document.querySelectorAll("#page-header-parent > .bg-transparent:not(.unfixed)");
+        this.negativeMarginElements = document.querySelectorAll(".negative-margin-top");
     }
 
+    /**
+     * sum all heights of elements in header
+     */
     calculateHeight() {
         let firstUnfixed = false;
         this.headerElements.forEach(element => {
             this.headerHeight += element.offsetHeight;
 
-            if(element.classList.contains("unfixed")){
+            // collect element heights until a fixed element is found
+            if(!element.classList.contains("unfixed")){
                 firstUnfixed = true
             }
 
@@ -38,12 +46,18 @@ export default class PlentyShopLTSModern {
         })
     }
 
+    /**
+     * set negative margin to elements
+     */
     setNegativeMargin() {
         this.negativeMarginElements.forEach((element) =>
             element.style.setProperty("margin-top", `${-this.headerHeight}px`, "important")
         );
     }
 
+   /**
+     * update header backgrounds
+     */
     updateHeaderBackgrounds() {
         const hasUnfixedElementsPassed = window.pageYOffset > this.unfixedElementsHeight;
 
